33bdcf7a60bb3e222ca350015a44b93bc34c8e23
Colm Tuite <colmtuite@gmail.com>
2014-11-09 18:13:58 +0000

    Add gitignore file

--- js/main.js
+++
@@ -1,7 +0,0 @@
-jQuery(function(){
-  $('.js-hoverHook').hoverHook();
-  $('.js-textEdit').textEdit();
-  $('.js-clickFunction').clickFunction();
-  $('.js-scrollTo').scrollFunction();
-  $('.js-rUI').responsiveUI();
-});
--- js/objects/textEdit.js
+++
@@ -1,9 +0,0 @@
-//<span class="js-textEdit" data-text="Show less">Show more</span>
-
-jQuery.fn.textEdit = function(){
-  $(this).click(function() {
-    var oldText = $(this).text();
-    var newText = $(this).data('text');
-    $(this).text(newText).data('text', oldText);
-  });
-};
